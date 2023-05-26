fcd9f38f8fdbeace75b30bd1e7164298a8541b43
winuss <niceyss@gmail.com>
2023-05-26 21:21:00 +0900

    feat: banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Banner() {
   return (
-    <div>
-      <h1>banner</h1>
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl-[700px]">
+      <Image
+        alt="banner"
+        src="https://links.papareact.com/0fm"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button
+          className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3
+          hover:shadow-xl active:scale-90 transition duration-150"
+        >
+          I'm flexible
+        </button>
+      </div>
     </div>
   );
 }
