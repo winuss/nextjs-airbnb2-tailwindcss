8f75dbf31a79d311c71dadbf99bd230fd9d7c505
winuss <niceyss@gmail.com>
2023-05-30 21:35:41 +0900

    footer text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -23,11 +23,11 @@ export default function Footer() {
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>Accessibility</p>
-        <p>This is not a real site</p>
-        <p>Its a pretty awesome site</p>
-        <p>Referrals accepted</p>
-        <p>Papafam</p>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Zero to Full Stack Hero</p>
+        <p>Hundreds of Students</p>
+        <p>Join Now</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY2</h5>
